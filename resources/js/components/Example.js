f318c751dd75c3f92019734fc53564ca6b2809df
fuadnafiz98 <fuadnafiz98@gmail.com>
2020-11-11 17:05:34 +0600

    :octocat: initial commit

---
+++ resources/js/components/Example.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Example() {
+    return (
+        <div className="container">
+            <h2 className="bg-red-200 font-sans">Hello world</h2>
+        </div>
+    );
+}
+
+export default Example;
